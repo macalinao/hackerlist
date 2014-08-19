bf9c16a74c7872cacfbbcb293f8b52e5eb5cd639
Ian Macalinao <me@ian.pw>
2014-08-19 13:27:20 -0500

    Add logic for hacker controller

--- client/app/hacker/hacker.controller.js
+++ client/app/hacker/hacker.controller.js
@@ -1,6 +1,9 @@
 'use strict';
 
 angular.module('hackerlistApp')
-  .controller('HackerCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  .controller('HackerCtrl', function ($scope, $stateParams, $http) {
+    $scope.hacker = {};
+    $http.get('/api/hacker/' + $stateParams.hacker).then(function(data) {
+      $scope.hacker = data.data;
+    });
   });
